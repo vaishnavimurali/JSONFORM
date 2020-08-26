4af8a84bc23075230d9d696267ec2373f048ddaf
vaishnavimurali <48913700+vaishnavimurali@users.noreply.github.com>
2020-08-26 19:30:34 +0530

    INITIAL COMMIT

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "jsonform",
+  "version": "2.2.1",
+  "main": "./lib/jsonform.js",
+  "user": "jsonform",
+  
+  "description": "Client-side JavaScript library that generates HTML forms from structured data models expressed using a JSON schema, possibly completed by a form layout description.",
+  "keywords": [
+    "JSON",
+    "schema",
+    "HTML5",
+    "form"
+  ],
+  "maintainers": [
+    {
+      "name": "grootan",
+      "email": "foo@bob.in",
+      "web": "https://www.joshfire.com"
+    },
+    
+  ],
+  "license": "MIT"
+}
